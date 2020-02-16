7530d5658dd3480bb6f3da831ca4a5ff610183d2
indev <indevprof@gmail.com>
2020-02-16 21:29:16 +0300

    -- merge

--- frontend/src/app/devtype/devtype.component.ts
+++ frontend/src/app/devtype/devtype.component.ts
@@ -13,7 +13,41 @@ export class DevtypeComponent implements OnInit {
     'web': {
       title: 'Web',
       note: 'Прекрасные и удобные веб-приложения на все случаи жизни',
+      img: '../../assets/img/web.svg',
+      imgsize: '12em',
+    },
+    'mobile': {
+      title: 'Mobile',
+      note: 'Прекрасные и удобные веб-приложения на все случаи жизни',
+      img: '../../assets/img/mobile.svg',
+      imgsize: '12em',
+    },
+    'desktop': {
+      title: 'Desktop',
+      note: 'Прекрасные и удобные веб-приложения на все случаи жизни',
+      img: '../../assets/img/desktop.svg',
+      imgsize: '12em',
+    },
+    'system': {
+      title: 'System',
+      note: 'Прекрасные и удобные веб-приложения на все случаи жизни',
+      img: '../../assets/img/system.svg',
+      imgsize: '12em',
+    },
+
+    'softdev': {
+      title: 'Software Development',
+      note: 'Прекрасные и удобные веб-приложения на все случаи жизни',
+      img: '../../assets/img/softdev-2.svg',
+      imgsize: '15em',
+    },
+    'integration': {
+      title: 'Integration',
+      note: 'Прекрасные и удобные веб-приложения на все случаи жизни',
+      img: '../../assets/img/integration.svg',
+      imgsize: '15em',
     }
+
   };
 
   constructor() { }
