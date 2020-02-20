daa0456bc276973012b046486dbd5570d977d307
indevprof <indevprof@gmail.com>
2020-02-20 15:26:00 +0300

    -- merge

--- frontend/src/app/pages/portfolio-page/portfolio.component.ts
+++ frontend/src/app/pages/portfolio-page/portfolio.component.ts
@@ -34,30 +34,30 @@ export class PortfolioPageComponent implements OnInit {
       img: 'assets/img/web-red.svg',
       imgsize: '10em',
     },
-    'cpn-ide-2': {
-      title: 'CPN IDE',
-      note: 'Desktop GUI application for editing, testing and simulating Colored Petri Nets',
-      img: 'assets/img/desktop-red.svg',
-      imgsize: '10em',
-    },
-    'tradein-2': {
-      title: 'Smartphone Diagnostic',
-      note: 'Smartphone diagnostic mobile applications',
-      img: 'assets/img/mobile-red.svg',
-      imgsize: '10em',
-    },
-    'subtitles-2': {
-      title: 'Automatic Subtitles',
-      note: 'Automatic subtitles service',
-      img: 'assets/img/system-red.svg',
-      imgsize: '10em',
-    },
-    'stc-cloud-2': {
-      title: 'Speech Cloud Portal',
-      note: 'Speech Cloud processing service',
-      img: 'assets/img/web-red.svg',
-      imgsize: '10em',
-    },
+    // 'cpn-ide-2': {
+    //   title: 'CPN IDE',
+    //   note: 'Desktop GUI application for editing, testing and simulating Colored Petri Nets',
+    //   img: 'assets/img/desktop-red.svg',
+    //   imgsize: '10em',
+    // },
+    // 'tradein-2': {
+    //   title: 'Smartphone Diagnostic',
+    //   note: 'Smartphone diagnostic mobile applications',
+    //   img: 'assets/img/mobile-red.svg',
+    //   imgsize: '10em',
+    // },
+    // 'subtitles-2': {
+    //   title: 'Automatic Subtitles',
+    //   note: 'Automatic subtitles service',
+    //   img: 'assets/img/system-red.svg',
+    //   imgsize: '10em',
+    // },
+    // 'stc-cloud-2': {
+    //   title: 'Speech Cloud Portal',
+    //   note: 'Speech Cloud processing service',
+    //   img: 'assets/img/web-red.svg',
+    //   imgsize: '10em',
+    // },
   }
 
   constructor() { }
