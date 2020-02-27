9ae593cb4e48fb244068f1f43fb596cd568969ee
indevprof <indevprof@gmail.com>
2020-02-27 09:12:07 +0300

    -- play video on load

--- frontend/src/app/pages/home-page-splash/home-page-splash.component.ts
+++ frontend/src/app/pages/home-page-splash/home-page-splash.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home-page-splash',
-  templateUrl: './home-page-splash.component.html',
-  styleUrls: ['./home-page-splash.component.scss']
+  selector: "app-home-page-splash",
+  templateUrl: "./home-page-splash.component.html",
+  styleUrls: ["./home-page-splash.component.scss"]
 })
 export class HomePageSplashComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  startPlayback() {
+    setTimeout(() => this.toggleMute(), 100);
   }
 
+  toggleMute() {
+    const video: any = document.getElementById("videoElement");
+
+    console.log("toggleMute(), video.muted = ", video.muted);
+
+    // if (video.muted) {
+    //   video.muted = false;
+    // } else {
+    if (!video.muted) {
+      // debugger;
+      video.muted = true;
+      video.play();
+    }
+  }
 }
